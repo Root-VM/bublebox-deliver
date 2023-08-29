f3f596ff38046a96ff8cc3641dab2956f03f4faa
Vlad <ngvmtext@gmail.com>
2023-08-29 12:14:38 +0300

    feat: changes

--- assets/ios.transition-legacy-bb7fe209.js
+++ assets/ios.transition-legacy-bb7fe209.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ff265ca6.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.m,n=e.o}],execute:function(){
+System.register(["./index-legacy-e9fa31d3.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.m,n=e.o}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- assets/md.transition-legacy-11fabd4a.js
+++ assets/md.transition-legacy-11fabd4a.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ff265ca6.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.m}],execute:function(){
+System.register(["./index-legacy-e9fa31d3.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.m}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
