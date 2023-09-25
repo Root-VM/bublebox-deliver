6f830dbe6a9bb0e2d8c5bf83342f6a70407b02e3
Vlad <ngvmtext@gmail.com>
2023-09-25 08:26:35 +0300

    last changes

--- assets/md.transition-legacy-bbeff04e.js
+++ assets/md.transition-legacy-bbeff04e.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-e9fa31d3.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.m}],execute:function(){
+System.register(["./index-legacy-5c7e09c2.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.m}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
